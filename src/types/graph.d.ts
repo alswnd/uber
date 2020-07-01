f9afc5d5f128ab65088c7878e401ef06805c266c
alswnd <alswnd0427@gmail.com>
2020-07-01 20:04:19 +0900

    Typechecking in graphql args

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,12 +1,16 @@
-export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello: String!\n}\n\n# these types must be known by typescript.\ntype Greeting {\n  text: String!\n  error: Boolean!\n}\n"];
+export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello(name: String): SayHelloResponse!\n}\n\n# these types must be known by typescript.\ntype SayHelloResponse {\n  text: String!\n  error: Boolean!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
   sayBye: string;
-  sayHello: string;
+  sayHello: SayHelloResponse;
 }
 
-export interface Greeting {
+export interface SayHelloQueryArgs {
+  name: string | null;
+}
+
+export interface SayHelloResponse {
   text: string;
   error: boolean;
 }
