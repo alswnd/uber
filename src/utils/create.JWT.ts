babd30bc503df8f0898c2a065ecaaef44fb77284
alswnd <alswnd0427@gmail.com>
2020-07-05 23:02:39 +0900

    Create JWT

---
+++ src/utils/create.JWT.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const JWT_TOKEN = JSON.stringify(process.env.JWT_TOKEN);
+
+/**
+ * @param {number} id
+ * @returns {string} token
+ */
+const createJWT = (id: number): string => {
+  const token = jwt.sign(
+    {
+      id: id,
+    },
+    JWT_TOKEN
+  );
+
+  return token;
+};
+
+export default createJWT;
