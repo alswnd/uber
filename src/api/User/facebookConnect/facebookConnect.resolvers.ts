67194cbec24e7213453339f76ff867024f6ea936
alswnd <alswnd0427@gmail.com>
2020-07-05 17:12:07 +0900

    Create FacebookConnect resolver

---
+++ src/api/User/facebookConnect/facebookConnect.resolvers.ts
@@ -0,0 +1,74 @@
+import { Resolvers } from "src/types/resolvers";
+import {
+  FacebookConnectMutationArgs,
+  FacebookConnectResponse,
+} from "src/types/graph";
+import User from "src/entities/User";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    FacebookConnect: async (
+      _,
+      args: FacebookConnectMutationArgs
+    ): Promise<FacebookConnectResponse> => {
+      const { fbId } = args;
+      try {
+        /**
+         * @const existingUser
+         * @description check if the facebook id already exists
+         * @method findOne() : method in BaseEntity
+         * @returns User | undefined
+         */
+        const existingUser = await User.findOne({ fbId });
+
+        // if the user already exists
+        if (existingUser) {
+          return {
+            ok: true,
+            error: null,
+            token: "" /** @todo */,
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null,
+        };
+      }
+
+      /**
+       * @description if the user is new one, create new user.
+       * the reason why it is outside of above try {}, it is because so complex.
+       */
+      try {
+        /**
+         * @param ...args : get all arguments. it contains firstName, lastName, fbID...
+         *
+         * @method create() : method in BaseEntity
+         * @returns User | undefined
+         *
+         * @method save() : method in BaseEntity
+         */
+        await User.create({
+          ...args,
+          profilePhoto: `https://graph.facebook.com/${fbId}/picture?type=square`,
+        }).save();
+
+        return {
+          ok: true,
+          error: null,
+          token: "" /** @todo */,
+        };
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null,
+        };
+      }
+    },
+  },
+};
+
+export default resolvers;
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -31,9 +31,9 @@ class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: "text", unique: true })
+  @Column({ type: "text", nullable: true })
   @IsEmail()
-  email: string;
+  email: string | null;
 
   @Column({ type: "boolean", default: false })
   verifiedEmail: boolean;
@@ -44,7 +44,7 @@ class User extends BaseEntity {
   @Column({ type: "text" })
   lastName: string;
 
-  @Column({ type: "int" })
+  @Column({ type: "int", nullable: true })
   age: number;
 
   @Column({ type: "text" })
@@ -79,6 +79,9 @@ class User extends BaseEntity {
 
   @Column({ type: "double precision" })
   lastOrientation: number;
+
+  @Column({ type: "text", nullable: true })
+  fbId: string; // facebook id
 
   @ManyToOne((type) => Chat, (chat) => chat.participants)
   chat: Chat;
