da1fe1d5421a27d1eb520ff773bc8ae97e549068
alswnd <alswnd0427@gmail.com>
2020-07-06 00:11:21 +0900

    Custom auth middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import helmet from "helmet";
 import logger from "morgan";
 import schema from "./schema";
+import decodeJWT from "./utils/decode.JWT";
 
 class App {
   public app: GraphQLServer;
@@ -18,6 +19,28 @@ class App {
     this.app.express.use(cors());
     this.app.express.use(logger("dev"));
     this.app.express.use(helmet());
+    this.app.express.use(this.jwt);
+  };
+
+  /**
+   *
+   * @param req header
+   * @param res express gives
+   * @param next express gives
+   */
+  private jwt = async (req, res, next): Promise<void> => {
+    /**
+     * @name X-JWT name is custom.
+     */
+    const token = req.get("X-JWT");
+
+    if (token) {
+      const user = await decodeJWT(token);
+      console.log(user);
+    }
+
+    // go to next middleware
+    next();
   };
 }
 
