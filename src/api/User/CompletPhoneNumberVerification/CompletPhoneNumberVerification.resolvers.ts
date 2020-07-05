f8b91452e308c5954836dd62fabd085c41f704fc
alswnd <alswnd0427@gmail.com>
2020-07-05 19:46:10 +0900

    Completing phone number...

---
+++ src/api/User/CompletPhoneNumberVerification/CompletPhoneNumberVerification.resolvers.ts
@@ -0,0 +1,78 @@
+import { Resolvers } from "../../../types/resolvers";
+import {
+  CompletPhoneNumberVerificationResponse,
+  CompletPhoneNumberVerificationMutationArgs,
+} from "../../../types/graph";
+import Verification from "src/entities/Verification";
+import User from "src/entities/User";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    CompletPhoneNumberVerification: async (
+      _,
+      args: CompletPhoneNumberVerificationMutationArgs
+    ): Promise<CompletPhoneNumberVerificationResponse> => {
+      const { phoneNumber, key } = args;
+
+      try {
+        // find verification with phone number
+        const verification = await Verification.findOne({
+          payload: phoneNumber,
+          key,
+        });
+
+        // if verification not exists
+        if (!verification) {
+          return {
+            ok: false,
+            error: "Verification key not valid",
+            token: null,
+          };
+        } else {
+          verification.verified = true;
+          verification.save();
+        }
+
+        // if verification exists, return nothing so continue at below try{}
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null,
+        };
+      }
+
+      //if verification exists
+      try {
+        const user = await User.findOne({ phoneNumber });
+
+        if (user) {
+          // when user exists
+          user.verifiedPhoneNumber = true;
+
+          user.save();
+          return {
+            ok: true,
+            error: null,
+            token: "verification and user exists",
+          };
+        } else {
+          // phonenumber verified and user not exists
+          return {
+            ok: true,
+            error: null,
+            token: null,
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null,
+        };
+      }
+    },
+  },
+};
+
+export default resolvers;
